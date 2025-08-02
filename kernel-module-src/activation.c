c9b737cd1ab28a4b26d767f575d34f14018e37df
StefanoBelli <stefano9913@gmail.com>
2025-08-02 21:11:21 +0200

    passwd auth with basic SHA256 hashing

--- kernel-module-src/activation.c
+++ kernel-module-src/activation.c
@@ -1,25 +1,81 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/cred.h>
+#include <crypto/hash.h>
 #include <activation.h>
 
+static struct crypto_shash *sha256_shash = NULL;
+static char *auth_passwd;
+
+static int auth_check(const char* passwd);
+
+/* activate/deactivate snapshot */
+
 static int activate_snapshot(const char* dev_name, const char* passwd) {
-    const struct cred* task_cred = current->cred;
-    if(task_cred->euid.val != 0) {
-        return -EPERM;
+    int auth_check_rv = auth_check(passwd);
+    if(auth_check_rv != 0) {
+        return auth_check_rv;
     }
 
     return 0;
 }
 
 static int deactivate_snapshot(const char* dev_name, const char* passwd) {
+    int auth_check_rv = auth_check(passwd);
+    if(auth_check_rv != 0) {
+        return auth_check_rv;
+    }
+
+
+    return 0;
+}
+
+/* compute sha256 */
+
+static int hash_sha256(const char* data, size_t datalen, char* output) {
+    struct shash_desc *desc;
+
+    size_t size = sizeof(struct shash_desc) + crypto_shash_descsize(sha256_shash);
+    desc = kmalloc(size, GFP_KERNEL);
+    if (desc == NULL) {
+        return -ENOMEM;
+    }
+    
+    desc->tfm = sha256_shash;
+
+    return crypto_shash_digest(desc, data, datalen, output);
+}
+/* password cmp */
+
+static int password_cmp(const char* passwd) {
+    if(sha256_shash != NULL) {
+        char hashed_passwd[32];
+        int rv = hash_sha256(passwd, strlen(passwd), hashed_passwd);
+        if(rv != 0) {
+            pr_err("hash_sha256 has failed: %d\n", rv);
+            return rv;
+        }
+        return memcmp(hashed_passwd, auth_passwd, 32);
+    } else {
+        return strcmp(passwd, auth_passwd);
+    }
+}
+
+/* auth check */
+
+static int auth_check(const char* passwd) {
     const struct cred* task_cred = current->cred;
     if(task_cred->euid.val != 0) {
         return -EPERM;
     }
+    
+    if(password_cmp(passwd) != 0) {
+        return -EACCES;
+    }
 
     return 0;
 }
+
 
 /* sysfs-related things below... */
 
@@ -93,6 +149,8 @@ static const struct kobj_attribute deactivate_kobj_attribute = (struct kobj_attr
     }
 };
 
+extern char* activation_ct_passwd;
+
 int setup_activation_mechanism_via_sysfs(void) {
     struct kobject *this_module_kobj = &THIS_MODULE->mkobj.kobj;
 
@@ -104,11 +162,41 @@ int setup_activation_mechanism_via_sysfs(void) {
         sysfs_remove_file(this_module_kobj, &activate_kobj_attribute.attr);
         return -1;
     }
+    
+    sha256_shash = crypto_alloc_shash("sha256", 0, 0);
+    if (!IS_ERR(sha256_shash)) {
+        auth_passwd = kmalloc(32, GFP_KERNEL);
+        if(auth_passwd == NULL) {
+            crypto_free_shash(sha256_shash);
+            return -ENOMEM;
+        }
+
+        if(hash_sha256(activation_ct_passwd, strlen(activation_ct_passwd), auth_passwd) != 0) {
+            crypto_free_shash(sha256_shash);
+            kfree(auth_passwd);
+            return -1;
+        }
+    } else {
+        pr_warn("unable to allocate sha256 shash: %ld\n", PTR_ERR(sha256_shash));
+        auth_passwd = kmalloc(strlen(activation_ct_passwd), GFP_KERNEL);
+        if(auth_passwd == NULL) {
+            return -ENOMEM;
+        }
+        memcpy(auth_passwd, activation_ct_passwd, strlen(activation_ct_passwd));
+    }
+
+    memset(activation_ct_passwd, 0, strlen(activation_ct_passwd));
 
     return 0;
 }
 
 void destroy_activation_mechanism_via_sysfs(void) {
+    kfree(auth_passwd);
+
+    if (sha256_shash != NULL) {
+        crypto_free_shash(sha256_shash); 
+    }
+
     struct kobject *this_module_kobj = &THIS_MODULE->mkobj.kobj;
 
     sysfs_remove_file(this_module_kobj, &activate_kobj_attribute.attr);
--- kernel-module-src/main.c
+++ kernel-module-src/main.c
@@ -2,6 +2,9 @@
 #include <linux/module.h>
 #include <activation.h>
 
+char* activation_ct_passwd = NULL;
+module_param_named(actpasswd, activation_ct_passwd, charp, 0);
+
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Block device snapshot");
 MODULE_AUTHOR("Stefano Belli");
@@ -11,6 +14,11 @@ void __exit exit_blkdev_snapshot_module(void);
 
 
 int __init init_blkdev_snapshot_module(void) {
+    if(activation_ct_passwd == NULL || strlen(activation_ct_passwd) == 0) {
+        pr_err("no password was provided\n");
+        return -ENODATA;
+    }
+
     return setup_activation_mechanism_via_sysfs();
 }
 
