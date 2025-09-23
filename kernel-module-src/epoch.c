d7d6956adaaa50d195af99038e2e7b794a26ae29
StefanoBelli <stefano9913@gmail.com>
2025-09-23 17:23:35 +0200

    Changing umount(2) kretprobe

--- kernel-module-src/epoch.c
+++ kernel-module-src/epoch.c
@@ -2,13 +2,16 @@
 #include <linux/blkdev.h>
 #include <linux/kprobes.h>
 #include <linux/version.h>
+#include <linux/fs.h>
 
 #include <epoch.h>
+#include <get-loop-backing-file.h>
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 2, 0)
 #define KRP_MOUNT_SYMBOL_NAME "do_move_mount"
-#define KRP_UMOUNT_SYMBOL_NAME "do_umount"
 #endif
+
+#define KRP_UMOUNT_SYMBOL_NAME "path_umount"
 
 static struct kretprobe krp_mount;
 static struct kretprobe krp_umount;
@@ -36,9 +39,11 @@ static int mount_handler(struct kretprobe_instance* krp_inst, struct pt_regs* re
 }
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 2, 0)
 static int umount_entry_handler(struct kretprobe_instance* krp_inst, struct pt_regs* regs) {
-	pr_err("U-MOUNT ENTRY\n");
+	struct path *path = (struct path*) regs->di;
+	struct block_device *bd = path->mnt->mnt_sb->s_bdev;
+	
+	pr_err("U-MOUNT ENTRY %s\n", get_loop_backing_file(bd));
 	return 0;
 }
 
@@ -46,7 +51,6 @@ static int umount_handler(struct kretprobe_instance* krp_inst, struct pt_regs* r
 	pr_err("U-MOUNT EXIT\n");
 	return 0;
 }
-#endif
 
 int setup_epoch_mgmt(void) {
 	krp_mount.entry_handler = mount_entry_handler;
